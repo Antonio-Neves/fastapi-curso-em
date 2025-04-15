9f3b0f5500366234dac067e1b867df1669182c7c
Antonio-Neves <antoniocalheirosneves@gmail.com>
2025-04-15 14:45:27 +0100

    Created test for create user

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,16 +1,32 @@
-from fastapi.testclient import TestClient
-from fast_zero.app import app
-from fast_zero.schemas import Message
-
+# import pytest
+# from fastapi.testclient import TestClient
 from http import HTTPStatus
 
+# from fast_zero.app import app
 
-client = TestClient(app)
+
+def test_root_must_return_ok_and_obrigado(client):
+
+    response = client.get('/')  # Action
+
+    assert response.status_code == HTTPStatus.OK  # Assert
+    assert response.json() == {'message': 'Obrigado meu Deus'}  # Assert
 
 
-def test_root_must_return_ok_and_obrigado():
-    client = TestClient(app)
-    response = client.get('/')
+def test_create_user(client):
 
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'Obrigado meu Deus'}
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'Deus',
+            'email': 'test@test.com',
+            'password': 'pasword01'
+        }
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'Deus',
+        'email': 'test@test.com',
+        'id': 1
+    }
