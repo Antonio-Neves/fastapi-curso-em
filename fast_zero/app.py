e243a93ad823587a56e3bc22ee36f966b90173ac
Antonio-Neves <antoniocalheirosneves@gmail.com>
2025-04-15 14:46:14 +0100

    Created function to creat user

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,13 +1,26 @@
 from fastapi import FastAPI
 from http import HTTPStatus
 
-from fast_zero.schemas import Message
+from fast_zero.schemas import Message, UserSchema, UserDB, UserPublic
 
 
 app = FastAPI()
 
+database = []
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Obrigado meu Deus'}
 
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+
+    user_with_id = UserDB(
+        id =len(database) + 1,
+        **user.model_dump()
+    )
+
+    database.append(user_with_id)
+
+    return user_with_id
